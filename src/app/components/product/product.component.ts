05b21ff39b851a6f52fbdf0d8e6ef62a2a616c03
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-11-07 11:32:43 +0100

    page single-product et 2 boutons par item

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  slug: string | undefined
+  product: Product | undefined
+  productSub: Subscription | undefined
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    window.scrollTo(0,0)
+    this.slug = this.route.snapshot.params['slug']
+    this.productSub = this.productService.getProducts()
+    .subscribe({
+      next: (products: Product[]) => {
+        this.product = products.filter(p => p.slug === this.slug)[0]
+      },
+      error: (error: any)=> {
+        console.log("Erreur : ", error)
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+      this.productSub?.unsubscribe()
   }
 
 }
