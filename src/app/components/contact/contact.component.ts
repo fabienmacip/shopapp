fbd65388f68c8b35686b57375afc9c8823053222
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2023-03-25 16:27:37 +0100

    ContactForm -> FormGroup et liaison de données

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup
+  firstname: FormControl
+  email: FormControl
+  subject: FormControl
+  phone : FormControl
+  message : FormControl
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+      this.firstname = this.fb.control("",[Validators.required, Validators.minLength(3)])
+      this.email = this.fb.control("",[Validators.required, Validators.email])
+      this.subject = this.fb.control("",[Validators.required, Validators.minLength(10)])
+      this.phone = this.fb.control("",[Validators.required])
+      this.message = this.fb.control("",[Validators.required, Validators.minLength(20)])
+
+      this.contactForm = this.fb.group({
+        firstname: this.firstname,
+        email: this.email,
+        subject: this.subject,
+        phone: this.phone,
+        message: this.message,
+      })
+  }
 
   ngOnInit(): void {
     window.scrollTo(0,0)
+  }
+
+  handleSubmit(){
+    console.log(this.contactForm)
   }
 
 }
